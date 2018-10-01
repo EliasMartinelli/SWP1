4e61f718786fe7d01e6105d2946e39c006887d86
Uganda Knuckles <43405993+EliasMartinelli@users.noreply.github.com>
2018-10-01 16:59:37 +0200

    Create tag1_3.c

---
+++ tag1_3.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+	int countUpCounter = 1;
+
+	while (countUpCounter <= 100) {
+		printf("Counter: %d\n", countUpCounter);
+		countUpCounter++;
+	}
+	printf("Takeoff");
+	
+  return 0;
+}
