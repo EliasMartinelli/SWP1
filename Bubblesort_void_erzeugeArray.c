1372aa7881ff6746bbba39bbdfeeb096e0d86dfb
Uganda Knuckles <43405993+EliasMartinelli@users.noreply.github.com>
2018-12-12 09:15:39 +0100

    Update Bubblesort_void_erzeugeArray.c

--- Bubblesort_void_erzeugeArray.c
+++ Bubblesort_void_erzeugeArray.c
@@ -1,25 +1,59 @@
 #include <stdio.h>
-void erzeugeArray(int *, int);
-void zeigeArrayInConsole(int *, int);
-void dreiEcksTausch(int *, int);
-void bubbleSort(int *, int);
 
-int main()
-{
-	int anz = 10;
-	int z[anz];
-	
-	void erzeugeArray(z, anz);
-	    printf("Array mit %d zufälligen Zahlen:\n ", anz);
+void erzeugeArray (int *, int);
 
- 	return 0;
-}
+void zeigeArrayInConsole (int *, int);
 
-void erzeugeArray(int * array, int anz)
-{    
-    for (int i = 0; i < anz; i++) {
-        array[i] = rand()%100;
-    }
+void dreiEcksTausch (int *, int);
+
+void bubbleSort (int *, int);
+
+ 
+    int main () 
+    {
+      
+        int anz = 10;
+          
+        int z[anz];
+          
+         
+        erzeugeArray (z, anz);
+          
+        printf ("Array mit %d zufälligen Zahlen:\n ", anz);
+          
+        zeigeArrayInConsole (z, anz);
+          
+        printf ("\n ", anz);
+          
+        return 0;
     
-    return 0;
-}
+    }
+
+
+ 
+    void erzeugeArray (int *array, int anz) 
+    {
+      
+        for (int i = 0; i < anz; i++)
+        { 
+            array[i] = rand () % 100;
+        } 
+ 
+        // array[i] = (1, 2, 3, 4, 5, 6);
+        
+        return 0;
+
+    }
+
+
+ 
+    void zeigeArrayInConsole (int *array, int anz) 
+    {
+      
+        for (int i = 0; i < anz; i++)
+        {
+            printf ("%d ,", array[i]);
+            
+        } 
+        return 0;
+    }
