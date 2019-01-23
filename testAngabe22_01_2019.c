83a68aad169e1cb16bf21e2963859bf9467b85c7
Uganda Knuckles <43405993+EliasMartinelli@users.noreply.github.com>
2019-01-23 08:27:33 +0100

    Create testAngabe22_01_2019.c

---
+++ testAngabe22_01_2019.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void fillArray (int*, int, int, int);
+void printArray (int*, int, int);
+int getMin (int*, int);
+int getMax (int*, int);
+void buildNewArray (int*, int, int, int);
+
+
+int main(){
+    
+    int anzArrayElemente = 53;
+    int unterGrenze = 300, oberGrenze = 500;
+    int min, max;
+    int anzSpalten = 12;
+    
+    return 0;
+}
+
+int getMin (int*x, int anz){
+    
+}
+
+int getMax (int*x, int anz){
+    
+}
+
+int fillArray(int*x int anz, int uG, int oG){
+    
+}
+
+int printArray (int*x, int anzSpalten){
+    
+}
+
+void buildNewArray (int*x int anz, int min, int max){
+
+}
